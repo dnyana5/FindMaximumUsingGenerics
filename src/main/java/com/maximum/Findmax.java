89a46f0ea0b49545523571f8c1b48d5c1a84610a
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-01-06 20:11:35 +0530

    welcome message

---
+++ src/main/java/com/maximum/Findmax.java
@@ -0,0 +1,10 @@
+package com.maximum;
+
+import java.util.Scanner;
+
+public class Findmax {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome Test maximum code using generics");
+    }
+}
