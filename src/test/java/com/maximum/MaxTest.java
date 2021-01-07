0778a851e85b6042acb32847a0e525bea1a7816e
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-01-07 06:27:49 +0530

    Added:Test maximum number given at second  position

--- src/test/java/com/maximum/MaxTest.java
+++ src/test/java/com/maximum/MaxTest.java
@@ -10,6 +10,13 @@ public class MaxTest {
         Integer result =maxValue.max(9, 8, 3);
         Assert.assertSame(9, result);
     }
+
+    @Test
+    public void givenMaxNumberAtSecondPosition_ReturnMaximumValueSame() {
+        Findmax maxValue = new Findmax();
+        Integer result = maxValue.max(5,19,3);
+        Assert.assertSame(19,result);
+    }
 }
 
 
