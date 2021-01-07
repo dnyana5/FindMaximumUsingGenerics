0d990fa8bda57870e75472a214c4c15e738c4447
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-01-07 06:58:32 +0530

    Added:Method to find maximum float value

--- src/test/java/com/maximum/MaxTest.java
+++ src/test/java/com/maximum/MaxTest.java
@@ -5,23 +5,42 @@ import org.junit.Test;
 
 public class MaxTest {
     @Test
-    public void givenMaxNumberAtFirstPosition_ReturnMaximumValueSame() {
+    public void givenIntegerMaxNumberAtFirstPosition_ReturnMaximumValueSame() {
         Findmax maxValue=new Findmax();
         Integer result =maxValue.max(9, 8, 3);
         Assert.assertSame(9, result);
     }
 
     @Test
-    public void givenMaxNumberAtSecondPosition_ReturnMaximumValueSame() {
+    public void givenIntegerMaxNumberAtSecondPosition_ReturnMaximumValueSame() {
         Findmax maxValue = new Findmax();
         Integer result = maxValue.max(5,19,3);
         Assert.assertSame(19,result);
     }
     @Test
-    public void givenMaxNumberAtThirdPosition_ReturnMaximumValueSame() {
+    public void givenIntegerMaxNumberAtThirdPosition_ReturnMaximumValueSame() {
         Findmax maxValue = new Findmax();
         Integer result = maxValue.max(16,19,35);
         Assert.assertSame(35,result);
+    }
+    @Test
+    public void givenFloatMaxNumberAtFirstPosition_ReturnMaximumValueSame() {
+        Findmax maxValue=new Findmax();
+        Float result =maxValue.max(7.0f, 4.0f, 2.0f);
+        Assert.assertEquals(7.0f, result,0);
+    }
+
+    @Test
+    public void givenFloatMaxNumberAtSecondPosition_ReturnMaximumValueSame() {
+        Findmax maxValue = new Findmax();
+        Float result = maxValue.max(5.0f,9.0f,3.0f);
+        Assert.assertEquals(9.0f,result,0);
+    }
+    @Test
+    public void givenFloatMaxNumberAtThirdPosition_ReturnMaximumValueSame() {
+        Findmax maxValue = new Findmax();
+        Float result = maxValue.max(6.0f,7.0f,8.0f);
+        Assert.assertEquals(8.0f,result,0);
     }
 }
 
